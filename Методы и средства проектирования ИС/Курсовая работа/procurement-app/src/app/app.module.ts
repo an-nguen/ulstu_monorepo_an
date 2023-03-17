7b0ac2a8f2fe7e530628396a66d150b8af50b595
An Nguyen <an-1258@outlook.com>
2023-03-18 02:45:34 +0400

    WIP Курсовая 02:45 18.03.2023

--- Методы и средства проектирования ИС/Курсовая работа/procurement-app/src/app/app.module.ts
+++ Методы и средства проектирования ИС/Курсовая работа/procurement-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PurchasesComponent } from './purchases/purchases.component';
 import { MaterialsComponent } from './materials/materials.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MaterialsComponent } from './materials/materials.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
